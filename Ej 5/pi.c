84e47ca8409df41f7cdcb135a8548aad3f0f3169
F A M <70080501+Kdrew6723145@users.noreply.github.com>
2023-04-26 00:07:39 -0400

    Update pi.c

--- Ej 5/pi.c
+++ Ej 5/pi.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
-#include <mpi.h>
+#include <stdlib.h>
 #include <math.h>
+#include <mpi.h>
+#include <omp.h>
 
-int main(int argc, char** argv) {
-    int num_procesos, id_proceso, i, n, local_n, rango_inicial, rango_final;
-    double termino, suma_local, suma_global;
+#define N 100000
+
+int main(int argc, char* argv[]) {
+    int rank, size, i;
+    double x, sum = 0.0, pi = 0.0;
+    double start, end;
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &num_procesos);
-    MPI_Comm_rank(MPI_COMM_WORLD, &id_proceso);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    n = 1000;
-    local_n = n / num_procesos;
-    rango_inicial = id_proceso * local_n;
-    rango_final = rango_inicial + local_n;
+    int chunk_size = N / size;
+    int chunk_start = rank * chunk_size;
+    int chunk_end = (rank + 1) * chunk_size;
 
-    suma_local = 0.0;
-    for (i = rango_inicial; i < rango_final; i++) {
-        termino = pow(-1, i) / (2 * i + 1);
-        suma_local += termino;
+    start = MPI_Wtime();
+    #pragma omp parallel for private(x) reduction(+:sum)
+    for (i = chunk_start; i < chunk_end; i++) {
+        x = (i + 0.5) / N;
+        sum += 4.0 / (1.0 + x*x);
     }
 
-    MPI_Reduce(&suma_local, &suma_global, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    double local_pi = sum / N;
 
-    if (id_proceso == 0) {
-        double pi = 4 * suma_global;
-        printf("El valor de Pi es aproximadamente: %f\n", pi);
+    MPI_Reduce(&local_pi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    end = MPI_Wtime();
+
+    if (rank == 0) {
+        printf("El resultado de Pi aprox es -> %.32f \n", pi);
+        printf("Tiempo de ejecucion %f segundos. \n",end-start);
     }
 
     MPI_Finalize();
-
     return 0;
 }
